54a7091e696280d3e8bfd218bd890305d7201181
waynekiprotich <waynekip123@icloud.com>
2026-04-15 16:38:48 +0300

    Work on Deck class

--- game/Deck.py
+++ game/Deck.py
@@ -1,63 +1,58 @@
+'''
+creates full 52-card deck
+shuffles cards randomly
+gives cards to players
+handles burn mechanic
+supports dealing system
+
+'''
+
+
 from Card import Card
 import random
 
 class Deck():
-    
-    def __init__(self):
-        # Creates a full 52-card deck
-        ranks = Card.RANKS
-        suites = Card.SUITES 
-        deck = []
 
-        # Nested loop builds every combination of rank + suit
+    def __init__(self):
+        ranks=Card.RANKS
+        suites=Card.SUITES
+        deck=[]
+
         for rank in ranks:
             for suite in suites:
-                card = Card(suite=suite, rank=rank)
+                card=Card(suite=suite,rank=rank)
                 deck.append(card)
-
-        self.deck = deck
-        # Stores deck in the object
-
+        self.deck=deck
 
     def shuffle(self):
-        newDeck = []
-        deck = self.deck
-
-        # Manually shuffles by randomly picking cards out of the deck
+        newDeck=[]
+        deck=self.deck
         while True:
-            if len(deck) == 1:
-                card = deck[0]
+            if len(deck)==1:
+                card=deck[0]
                 newDeck.append(card)
                 break
-            
-            n = random.randint(0, len(deck) - 1)
-            
-            card = deck[n]
+            n=random.randint(0,len(deck)-1)
+       
+            card=deck[n] 
             deck.pop(n)
             newDeck.append(card)
-            
-        self.deck = newDeck
-        # Replaces old deck with shuffled version
+        self.deck=newDeck
 
+    def print_deck(self):
+        deck=self.deck
+        print("Deck size is",len(deck))
+        print("..............")
+        for card in deck:
+            card.print_card()
+            print("-------------")
 
     def burn_card(self):
-        # Moves top card to bottom 
-        if len(self.deck) > 0:
-            top_card = self.deck.pop() 
-            self.deck.insert(0, top_card) 
-
+        top_card=self.deck[0]
+        self.deck.pop(0)
+        self.deck.append(top_card)
 
     def give_card(self):
-        # Deals a card from the top of the deck
-        if len(self.deck) > 0:
-            return self.deck.pop()
-        else:
-            print("No cards left in deck!")
-            return None
-
-
-
-if __name__ == "__main__":
-    d1 = Deck()
-    d1.shuffle()
-    # Creates and shuffles a deck for testing
+        top_card=self.deck[0]
+        self.deck.pop(0)
+        return top_card
